f75f5000e1d846d566b2b1bcc508f92ce0680ec9
hansbirkaur <121248404+hansbirkaur@users.noreply.github.com>
2022-12-22 13:34:05 -0500

    changes
    chages to hello

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is amazing"
+        value = "hello"
     }
     val text: LiveData<String> = _text
 }
